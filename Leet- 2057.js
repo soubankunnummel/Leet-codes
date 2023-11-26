7f712ecca149b922f0b4d8395b655132e2cd3b87
soubankunnummel <muhammedsoubankunnummel123@gmail.com>
2023-11-26 13:07:27 +0530

    Leet 2057

---
+++ Leet- 2057.js
@@ -0,0 +1,12 @@
+var smallestEqual = function (nums) {
+  for (let i = 0; i <= nums.length; i++) {
+    if (i % 10  === nums[i]) {
+      return i;
+    } 
+
+}
+return -1
+};
+
+const nums = [4, 3, 2, 1];
+console.log(smallestEqual(nums));
