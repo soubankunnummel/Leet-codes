d80d222272404b8022e8e7bbafe96b797d3fe1f6
soubankunnummel <muhammedsoubankunnummel123@gmail.com>
2023-11-28 09:08:59 +0530

    Leet 2108

---
+++ Leet  - 2108.js
@@ -0,0 +1,21 @@
+var firstPalindrome = function(words) {
+    // const arry = words.map((item) => [item])
+    for (const word of words) {
+        // console.log(word)
+        if(checkPalindrom(word))
+        return word
+    }
+    return ""
+};
+
+const checkPalindrom = (words) => {
+    for(let i = 0; i <= words.length/2 ; i++){
+            if(words[i] !== words[words.length - 1 -i]){
+                return false
+            }
+        }
+        return true
+}
+
+const  words =  ["notapalindrome","racecar"]
+console.log(firstPalindrome(words))
