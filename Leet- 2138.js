d9a40a86533974c239518a88a7b6f37e762d9ae9
soubankunnummel <muhammedsoubankunnummel123@gmail.com>
2023-12-01 20:03:14 +0530

    completed

--- Leet- 2138.js
+++ Leet- 2138.js
@@ -1,45 +1,18 @@
-// var divideString = function(s, k, fill) {
-//     let text = s.padEnd(Math.ceil(s.length/3)*3,fill)
-//     const ayy = text.match(/.{3}/g)
-//    return ayy
-// };
-// const s = "ctoyjrwtngqwt"
-// const  k = 3
-// const fill = "x"
-// console.log(divideString(s,k,fill))
-
-
-function partitionString(s, k, fill) {
+function divideString(string, k, fill) {
    const partitions = [];
-   let i = 0;
- 
-   while (i < s.length) {
-     const partition = s.slice(i, i + k);
- 
-     // Check if merging is possible
-     if (partitions.length >= 2 && partitions[partitions.length - 1].length + partition.length >= k) {
-       // Merge the last two groups
-       partitions[partitions.length - 2] += partition;
-       continue;
-     }
- 
-     // Fill the partition with 'fill' characters if necessary
+   for (let i = 0; i < string.length; i += k) {
+     let partition = string.slice(i, i + k);
      if (partition.length < k) {
        partition += fill.repeat(k - partition.length);
      }
- 
-     // Append the partition to the list
      partitions.push(partition);
- 
-     i += k;
    }
- 
    return partitions;
  }
  
- const s = "ctoyjrwtngqwt"
-const  k = 3
-const fill = "x"
-console.log(partitionString(s,k,fill))
-
-TODO // need to update
+ const string = "abcdefghi";
+ const k = 3;
+ const fill = "x";
+ 
+ const partitions = divideString(string, k, fill);
+ console.log(partitions); // Output: [ 'abc', 'def', 'ghi', 'jxx' ]
