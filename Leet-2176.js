c47f01e1626bd4ae1b1e0320d7390fca57a8b795
soubankunnummel <muhammedsoubankunnummel123@gmail.com>
2023-12-04 10:48:26 +0530

    Leet 2176

---
+++ Leet-2176.js
@@ -0,0 +1,18 @@
+var countPairs = function(nums, k) {
+
+    let count = 0
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i+1; j < nums.length; j++) {
+        
+            if(nums[i] === nums[j] && i * j % k == 0) count ++
+            
+            
+        }
+        
+        
+    }
+    return count
+    
+};
+console.log(countPairs([3,1,2,2,2,1,3], 2));
